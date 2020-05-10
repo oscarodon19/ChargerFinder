6abcca289f59da44732401b9536512cea1a9cc0f
Oscar Odon <oscarodon19@gmail.com>
2020-05-10 11:03:36 -0300

    Remove Pods directory from repository

--- Pods/Target Support Files/ChargerFinder/ChargerFinder-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ChargerFinder : NSObject
-@end
-@implementation PodsDummy_ChargerFinder
-@end
--- Pods/Target Support Files/Pods-ChargerFinderDemoApp/Pods-ChargerFinderDemoApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ChargerFinderDemoApp : NSObject
-@end
-@implementation PodsDummy_Pods_ChargerFinderDemoApp
-@end
